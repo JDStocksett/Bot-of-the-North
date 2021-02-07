8f58d09bcd5b81e38f860ad15fa3b6e3e23a9d1b
JDStocksett <jdstocksett@gmail.com>
2021-02-07 11:36:24 -0600

    Getting ready for Heroku

--- commands/clearchannel.js
+++ commands/clearchannel.js
@@ -1,6 +1,6 @@
 module.exports = async function (msg, args) {
 	if (msg.member.roles.cache.some(r=>["Volunteer", "Paizo Society", "Magnetrons"].includes(r.name))) {
-		if ((msg.channel.id !== '742919402814439535') && (msg.channel.id !== '742919603574931608') && (msg.channel.id !== '742916074294476813') && (msg.channel.id !== '742917901853655101') && (msg.channel.id !== '251095634625429504')) {
+		if ((msg.channel.id !== '742919402814439535') && (msg.channel.id !== '742919603574931608') && (msg.channel.id !== '742916074294476813') && (msg.channel.id !== '742917901853655101')) {//&& (msg.channel.id !== '251095634625429504')) {
 			let lines = 100;
 			if (args > 0){
 				lines = args;
